f638c849d83bd85c85877b03bd37575c49d6132f
hznirizhe <hznirizhe@corp.netease.com>
2018-03-18 16:28:10 +0800

    同步处理原始callback方式

--- app/form/origin.js
+++ app/form/origin.js
@@ -1,34 +1,8 @@
-import $ from 'Jquery'
+import api from '../dataAPI.js'
 
-function getAuthor(id, callback){
-    $.ajax("/data/author.json",{
-        author: id
-    }).done(function(result){
-        console.log('author data:', result)
-        callback(result);
-    })
-}
-
-function getArticle(id, callback){
-    $.ajax("/data/content.json",{
-        id: id
-    }).done(function(result){
-        console.log('content data:', result)
-        callback(result);
-    })
-}
-
-function getArticleList(callback){
-    $.ajax("/data/articles.json")
-    .done(function(result){
-        console.log('articles data:', result)
-        callback(result);
-    });
-}
-
-getArticleList(function(articles){
-    getArticle(articles[0].id, function(article){
-        getAuthor(article.authorId, function(author){
+api.getArticleList(function(articles){
+    api.getArticle(articles[0].id, function(article){
+        api.getAuthor(article.authorId, function(author){
             alert(author.email);
         })
     })
--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@
     可以打开其他注释查看不同的代码实现效果
  */
 
-// import origin from './form/origin.js'; // origin
+import origin from './form/origin.js'; // origin
 // import promiseInstance from './form/promise.js'; // promiseInstance 
 // import generatorPromiseInstance from './form/generator_promise.js'; // generatorPromiseInstance
-import async from './form/async.js'; // async await
+// import async from './form/async.js'; // async await
